949e7c824d1a562bae847dee0a2ed1d84607b8df
Luke Dutta <lukedutta9@gmail.com>
2021-02-18 15:53:59 -0800

    add support for es6 modules

--- source/example/example.test.js
+++ source/example/example.test.js
@@ -1,13 +1,14 @@
-const example = require('./example');
+import {jest} from '@jest/globals';
+import {hello, sum} from './example.js';
 
 // tests for hello (msg)
 test('print hello msg', () => {
     console.log = jest.fn();
-    example.hello('hello');
+    hello('hello');
     expect(console.log.mock.calls[0][0]).toBe('hello');
 });
 
 // tests for sum (a, b)
 test('adds 1 + 2 to equal 3', () => {
-    expect(example.sum(1, 2)).toBe(3);
+    expect(sum(1, 2)).toBe(3);
 });
