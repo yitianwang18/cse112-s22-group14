fffffa1a8ee006f2b37c8964b52b3f42dde1802f
hwaleh <hwaleh@ucsd.edu>
2021-02-18 20:18:17 -0800

    improve test

--- source/v1/js/notify.test.js
+++ source/v1/js/notify.test.js
@@ -1,9 +1,10 @@
+import {jest} from '@jest/globals';
 import notify from './notify';
 
 test('notif messages', function () {
         console.log = jest.fn();
         //test for starting pomo
-        this.notify.notify(0);
+        notify(0);
         expect(window.Notification.title.mock.toMatch(/Time to start the next work session!/));
     });
 
