1d49adbcdde306b9d83a27794f4eaf7166d7ebb2
James Zhao <jamezrollz@gmail.com>
2021-02-28 14:04:33 -0800

    tasklist unit tests

--- source/v1/tests/taskList.test.js
+++ source/v1/tests/taskList.test.js
@@ -72,6 +72,16 @@ test("TaskList static validateString", () => {
     let o_tasklist = new TaskList();
 
     const s_empty_string = "";
+    const s_whitespace = "   \t\n";
+    const s_normal = "task12345";
+    const s_normal_padded = " \t\ntask12345\t\n ";
+    const s_oversize = "".padStart(TaskList.N_MAX_TASK_LENGTH + 1, "A");
+    const s_oversize_padded = " \t\n" + "".padStart(TaskList.N_MAX_TASK_LENGTH - 2, "A") + "\t\n ";
     expect(TaskList.validateString(s_empty_string)).toBe(false);
+    expect(TaskList.validateString(s_whitespace)).toBe(false);
+    expect(TaskList.validateString(s_normal)).toBe(true);
+    expect(TaskList.validateString(s_normal_padded)).toBe(true);
+    expect(TaskList.validateString(s_oversize)).toBe(false);
+    expect(TaskList.validateString(s_oversize_padded)).toBe(true);
 });
 
