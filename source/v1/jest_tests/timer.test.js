be7129daab1f779a0d4df02efd0a3dd521db3a62
MZ-K <mzkhan@ucsd.edu>
2021-02-28 23:36:31 -0800

    tests refactored

--- source/v1/jest_tests/timer.test.js
+++ source/v1/jest_tests/timer.test.js
@@ -2,18 +2,18 @@ import { TimerDisplay } from "../js/timer.js";
 
 test('Test timer-display element initialization', () => {
     //Test custom element timer-display shape
-    let timer_disp = new TimerDisplay();
-    let timer_disp_wrapper = timer_disp.querySelector("div");
-    expect(timer_disp_wrapper.childElementCount).toBe(7);
+    let o_timer_disp = new TimerDisplay();
+    let o_timer_disp_wrapper = o_timer_disp.querySelector("div");
+    expect(o_timer_disp_wrapper.childElementCount).toBe(7);
 
     //Test timer-display.o_pomos_completed initialization
-    expect(timer_disp_wrapper.querySelector("h3").innerText).toBe("Pomodoros Completed:");
+    expect(o_timer_disp_wrapper.querySelector("h3").innerText).toBe("Pomodoros Completed:");
 
     //Test timer-diplay.o_pomo_images initialization
-    let pomo_images = timer_disp_wrapper.querySelectorAll("img");
+    let o_pomo_images = o_timer_disp_wrapper.querySelectorAll("img");
     const ACTUAL_POMO_NO_PATH = "http://localhost" + TimerDisplay.S_POMO_NO_PATH.substring(1);
     for (let i = 0; i < 4; ++i) {
-        expect(pomo_images[i].src).toBe(ACTUAL_POMO_NO_PATH);
+        expect(o_pomo_images[i].src).toBe(ACTUAL_POMO_NO_PATH);
     }
 });
 
@@ -29,7 +29,7 @@ test('Test padStart()', () => {
 });
 
 test('Test TimerDisplay.formatMilliTime', () => {
-    let millisecs;
+    let n_millisecs;
     let milTime = function (min, sec) {
         return (min * 60 + sec) * 1000;
     };
@@ -37,20 +37,20 @@ test('Test TimerDisplay.formatMilliTime', () => {
     expect(TimerDisplay.formatMilliTime(-1)).toBe('25:00');
     expect(TimerDisplay.formatMilliTime(0)).toBe('00:00');
 
-    millisecs = milTime(24, 59);
-    expect(TimerDisplay.formatMilliTime(millisecs)).toBe('24:59');
+    n_millisecs = milTime(24, 59);
+    expect(TimerDisplay.formatMilliTime(n_millisecs)).toBe('24:59');
 
-    millisecs = milTime(4, 5);
-    expect(TimerDisplay.formatMilliTime(millisecs)).toBe('04:05');
+    n_millisecs = milTime(4, 5);
+    expect(TimerDisplay.formatMilliTime(n_millisecs)).toBe('04:05');
 
-    millisecs = milTime(0, 59);
-    expect(TimerDisplay.formatMilliTime(millisecs)).toBe('00:59');
+    n_millisecs = milTime(0, 59);
+    expect(TimerDisplay.formatMilliTime(n_millisecs)).toBe('00:59');
 
-    millisecs = milTime(4, 0);
-    expect(TimerDisplay.formatMilliTime(millisecs)).toBe('04:00');
+    n_millisecs = milTime(4, 0);
+    expect(TimerDisplay.formatMilliTime(n_millisecs)).toBe('04:00');
 
-    millisecs = milTime(44, 59);
-    expect(TimerDisplay.formatMilliTime(millisecs)).toBe('44:59');
+    n_millisecs = milTime(44, 59);
+    expect(TimerDisplay.formatMilliTime(n_millisecs)).toBe('44:59');
 
 });
 
