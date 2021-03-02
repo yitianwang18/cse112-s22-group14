86a54a84a6c5dd74f9a50e760ce7ac3d1ad9412a
MZ-K <mzkhan@ucsd.edu>
2021-03-01 21:00:15 -0800

    Added tests for TaskList functionality onStartSession

--- source/v1/cypress_tests/cypress/integration/tests/taskListTests.js
+++ source/v1/cypress_tests/cypress/integration/tests/taskListTests.js
@@ -20,4 +20,44 @@ describe('Task List Tests', () => {
         cy.get('task-list').should('be.hidden');
 
     });
+
+    it('Test TaskList functionality when Session Starts / Ends', () => {
+
+        cy.get('task-list').should('be.hidden');
+        cy.get('#task-btn').should('be.enabled');
+
+        cy.get('timer-element').within(() => {
+            cy.get("#start-btn").trigger('click');
+        });
+
+        cy.get('task-list').should('be.hidden');
+        cy.get('#task-btn').should('be.disabled');
+
+        cy.get('timer-element').within(() => {
+            cy.get("#end-btn").trigger('click');
+        });
+
+        cy.get('task-list').should('be.hidden');
+        cy.get('#task-btn').should('be.enabled');
+
+        cy.get('#task-btn').trigger('click');
+
+        cy.get('task-list').should('be.visible');
+        cy.get('#task-btn').should('be.enabled');
+
+        cy.get('timer-element').within(() => {
+            cy.get("#start-btn").trigger('click');
+        });
+
+        cy.get('task-list').should('be.hidden');
+        cy.get('#task-btn').should('be.disabled');
+
+        cy.get('timer-element').within(() => {
+            cy.get("#end-btn").trigger('click');
+        });
+
+        cy.get('task-list').should('be.hidden');
+        cy.get('#task-btn').should('be.enabled');
+        
+    });
 });
