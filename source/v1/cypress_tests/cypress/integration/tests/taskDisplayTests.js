9fbbef80ace188b8b066ea7ac38c96bc5610e767
MZ-K <mzkhan@ucsd.edu>
2021-03-11 01:04:45 -0800

    Refactored UI tests

--- source/v1/cypress_tests/cypress/integration/tests/taskDisplayTests.js
+++ source/v1/cypress_tests/cypress/integration/tests/taskDisplayTests.js
@@ -1,60 +1,73 @@
 describe('Timer Container Tests', () => {
     beforeEach(() => {
         cy.visit('http://127.0.0.1:5500/source/v1/index.html');
+        cy.document().then((doc) => {
+            doc.querySelector('timer-element').toggleDebug();
+        });
     });
 
-    it('Test Timer Display Functionality when Session Starts and Ends', () => {
-        //cy.get('#task-btn').trigger('click');
-
+    it('Test Task Display Functionality when Session Starts and Ends', () => {
+        
+        //Two Tasks added to TaskList
+        cy.get('#task-btn').trigger('click');
         cy.get('task-list').within(() => {
             cy.get('#task-input').clear().type('First Test Task');
             cy.get('#add-btn').trigger('click');
-
             cy.get('#task-input').clear().type('Second Test Task');
             cy.get('#add-btn').trigger('click');
+            cy.get('#close-task').trigger('click');
         });
 
+        //Session started
         cy.get('timer-element').within(() => {
             cy.get("#start-btn").trigger('click');
         });
 
         cy.get('task-display').within(() => {
+            //Current task displays the first task
             cy.get("#current").should('contain', 'First Test Task');
+            //Next task displays the second task
             cy.get("#next").should('contain', 'Second Test Task');
         });
 
+        //Session ended
         cy.get('timer-element').within(() => {
             cy.get("#end-btn").trigger('click');
         });
 
+        //Task displays become hidden
         cy.get('task-display').should('be.hidden');
 
     });
 
-    it('Test Timer Display Functionality when Check button is pressed', () => {
-        //cy.get('#task-btn').trigger('click');
-
+    it('Test Task Display Functionality when Check button is pressed', () => {
+        
+        //3 Tasks added to TaskList
+        cy.get('#task-btn').trigger('click');
         cy.get('task-list').within(() => {
             cy.get('#task-input').clear().type('First Test Task');
             cy.get('#add-btn').trigger('click');
-
             cy.get('#task-input').clear().type('Second Test Task');
             cy.get('#add-btn').trigger('click');
-
             cy.get('#task-input').clear().type('Third Test Task');
             cy.get('#add-btn').trigger('click');
+            cy.get('#close-task').trigger('click');
         });
 
+        //Session started
         cy.get('timer-element').within(() => {
             cy.get("#start-btn").trigger('click');
         });
 
         cy.get('task-display').within(() => {
+            //Current task display -> first task & Next task Display -> second task
             cy.get("#current").should('contain', 'First Test Task');
             cy.get("#next").should('contain', 'Second Test Task');
 
+            //Check button clicked: Current task checked as complete
             cy.get("#check").trigger('click');
 
+            //Current task display -> second task & Next task Display -> third task
             cy.get("#current").should('contain', 'Second Test Task');
             cy.get("#next").should('contain', 'Third Test Task');
 
@@ -62,51 +75,60 @@ describe('Timer Container Tests', () => {
     });
 
     it('Test Check button is disabled during Break', () => {
-        cy.clock()
 
-        //cy.get('#task-btn').trigger('click');
-
+        //3 Tasks added to TaskList 
+        cy.get('#task-btn').trigger('click');
         cy.get('task-list').within(() => {
             cy.get('#task-input').clear().type('First Test Task');
             cy.get('#add-btn').trigger('click');
-
             cy.get('#task-input').clear().type('Second Test Task');
             cy.get('#add-btn').trigger('click');
-
             cy.get('#task-input').clear().type('Third Test Task');
             cy.get('#add-btn').trigger('click');
+            cy.get('#close-task').trigger('click');
         });
 
+        cy.clock();
+
+        //Session started
         cy.get('timer-element').within(() => {
             cy.get("#start-btn").trigger('click');
         });
 
         cy.get('task-display').within(() => {
+            //Work Cycle
+            //Current task display -> first task & Next task Display -> second task
             cy.get("#current").should('contain', 'First Test Task');
             cy.get("#next").should('contain', 'Second Test Task');
-
+            //Check button clicked: Current task checked as complete
             cy.get("#check").trigger('click');
-            
+            //Current task display -> second task & Next task Display -> third task
             cy.get("#current").should('contain', 'Second Test Task');
             cy.get("#next").should('contain', 'Third Test Task');
 
+            //Wait for Short Break
             cy.tick(3100);
             
+            //Short Break
+            //Current task display -> second task & Next task Display -> third task
             cy.get("#current").should('contain', 'Second Test Task');
             cy.get("#next").should('contain', 'Third Test Task');
-            
+            //Check button is disabled during a break
             cy.get("#check").should('be.disabled');
-
+            //Current task display -> second task & Next task Display -> third task
             cy.get("#current").should('contain', 'Second Test Task');
             cy.get("#next").should('contain', 'Third Test Task');
             
+            //Wait for the Work Cycle
             cy.tick(3100);
             
+            //Work Cycle
+            //Current task display -> second task & Next task Display -> third task
             cy.get("#current").should('contain', 'Second Test Task');
             cy.get("#next").should('contain', 'Third Test Task');
-
+            //Check button clicked: Current task checked as complete
             cy.get("#check").trigger('click');
-
+            //Current task display -> third task & Next task Display -> None(hidden)
             cy.get("#current").should('contain', 'Third Test Task');
             cy.get("#next").should('be.hidden');
 
@@ -114,46 +136,50 @@ describe('Timer Container Tests', () => {
     });
 
     it('Test Timer Display Functionality on Task Completion', () => {
-        cy.clock()
 
-        //cy.get('#task-btn').trigger('click');
-
+        //3 Tasks added to TaskLists
+        cy.get('#task-btn').trigger('click');
         cy.get('task-list').within(() => {
             cy.get('#task-input').clear().type('First Test Task');
             cy.get('#add-btn').trigger('click');
-
             cy.get('#task-input').clear().type('Second Test Task');
             cy.get('#add-btn').trigger('click');
-
             cy.get('#task-input').clear().type('Third Test Task');
             cy.get('#add-btn').trigger('click');
+            cy.get('#close-task').trigger('click');
         });
 
+        cy.clock();
+
+        //Session started
         cy.get('timer-element').within(() => {
             cy.get("#start-btn").trigger('click');
         });
 
+        //Work Cycle: Check button enabled
         cy.get('task-display').within(() => {
+            //Current task display -> first task & Next task Display -> second task
             cy.get("#current").should('contain', 'First Test Task');
             cy.get("#next").should('contain', 'Second Test Task');
-
+            //Check button clicked: Current task checked as complete
             cy.get("#check").trigger('click');
-
+            //Current task display -> second task & Next task Display -> third task
             cy.get("#current").should('contain', 'Second Test Task');
             cy.get("#next").should('contain', 'Third Test Task');
-
+            //Check button clicked: Current task checked as complete
             cy.get("#check").trigger('click');
-            
+            //Current task display -> third task & Next task Display -> None
             cy.get("#current").should('contain', 'Third Test Task');
             cy.get("#next").should('be.hidden');
-
+            //Check button clicked: Current task checked as complete
             cy.get("#check").trigger('click');
-
+            //Current task display -> None & Next task Display -> None (both hidden)
             cy.get("#current").should('be.hidden');
             cy.get("#next").should('be.hidden');
 
         });
 
+        //Session ended on completion of tasks
         cy.get('timer-element').within(() => {
             cy.get('#work-message').should('contain', 'Ready to focus?');
         });
--- source/v1/cypress_tests/cypress/integration/tests/timerContainerTests.js
+++ source/v1/cypress_tests/cypress/integration/tests/timerContainerTests.js
@@ -1,34 +1,71 @@
+import { TimerContainer } from "../../../../js/timerContainer.js";
 describe('Timer Container Tests', () => {
   beforeEach(() => {
     cy.visit('http://127.0.0.1:5500/source/v1/index.html');
+    cy.document().then((doc) => {
+      doc.querySelector('timer-element').toggleDebug();
+    });
   });
+  
+  it('Test Start Button does not start session when no tasks have been added', () => {
 
-  it('Test Start Button / Reset Button Toggle', () => {
-    
-    // TODO: Add test to see start button disabled when no tasks are inputted
-
+    //Session doesnot start when start button is clicked
+    cy.get('timer-element').within(() => {
+      cy.get('#work-message').should('contain', 'Ready to focus?');
+      cy.get('#start-btn').trigger('click');
+      cy.get('#work-message').should('contain', 'Ready to focus?');
+    });
+  
+    //A task is added
+    cy.get('#task-btn').trigger('click');
     cy.get('task-list').within(() => {
       cy.get('#task-input').clear().type('First Test Task');
       cy.get('#add-btn').trigger('click');
+      cy.get('#close-task').trigger('click');
+    });
+
+    //Session starts when start button is clicked
+    cy.get('timer-element').within(() => {
+      cy.get('#work-message').should('contain', 'Ready to focus?');
+      cy.get('#start-btn').trigger('click');
+      cy.get('#work-message').should('contain', 'Pomodoro - Start working!');
+    });
+  });
+
+  it('Test Start Button / Reset Button Toggle', () => {
+
+    //Task added so that session could be started for testing
+    cy.get('#task-btn').trigger('click');
+    cy.get('task-list').within(() => {
+      cy.get('#task-input').clear().type('First Test Task');
+      cy.get('#add-btn').trigger('click');
+      cy.get('#close-task').trigger('click');
     });
 
     cy.get('timer-element').within(() => {
-
+      
+      //Initially start button is visible and reset button is not
       cy.get('#start-btn').should('be.visible');
       cy.get('#reset-btn').should('be.hidden');
 
+      //Session started
       cy.get('#start-btn').trigger('click');
 
+      //Reset button is visible and start button is not
       cy.get('#start-btn').should('be.hidden');
       cy.get('#reset-btn').should('be.visible');
 
+      //Session reset
       cy.get('#reset-btn').trigger('click');
 
+      //Reset button is still visible and start button is not
       cy.get('#start-btn').should('be.hidden');
       cy.get('#reset-btn').should('be.visible');
 
+      //Session ended
       cy.get('#end-btn').trigger('click');
 
+      //Start button is visible and reset button is not
       cy.get('#start-btn').should('be.visible');
       cy.get('#reset-btn').should('be.hidden');
 
@@ -37,28 +74,37 @@ describe('Timer Container Tests', () => {
 
   it('Test Start Button functionality', () => {
 
+    
+
+    //Task added so that session could be started for testing
+    cy.get('#task-btn').trigger('click');
     cy.get('task-list').within(() => {
       cy.get('#task-input').clear().type('First Test Task');
       cy.get('#add-btn').trigger('click');
+      cy.get('#close-task').trigger('click');
     });
 
     cy.get('timer-element').within(() => {
       cy.clock();
 
+      //Session not started
       cy.get('#work-message').should('contain', 'Ready to focus?');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', -1);
         expect($el).to.have.attr('pomos-comp', 0);
       });
-
+      
+      //Start button clicked
       cy.get('#start-btn').trigger('click');
 
+      //Session started
       cy.get('#work-message').should('contain', 'Pomodoro - Start working!');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 3000);
         expect($el).to.have.attr('pomos-comp', 0);
       });
 
+      //Start button is not clickable if session is already started
       cy.get('#start-btn').should('be.hidden');
 
     });
@@ -66,16 +112,21 @@ describe('Timer Container Tests', () => {
 
   it('Test Reset Button functionality', () => {
 
+    //Task added so that session could be started for testing
+    cy.get('#task-btn').trigger('click');
     cy.get('task-list').within(() => {
       cy.get('#task-input').clear().type('First Test Task');
       cy.get('#add-btn').trigger('click');
+      cy.get('#close-task').trigger('click');
     });
 
     cy.get('timer-element').within(() => {
       cy.clock();
 
+      //Session started
       cy.get('#start-btn').trigger('click');
 
+      //Work cycle: Timer displays 3000ms
       cy.get('#work-message').should('contain', 'Pomodoro - Start working!');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 3000);
@@ -83,14 +134,17 @@ describe('Timer Container Tests', () => {
 
       cy.tick(2000);
 
+      //Timer runs down to 100000ms after a 1400000ms pause 
       cy.get('#work-message').should('contain', 'Pomodoro - Start working!');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 1000);
       });
 
+      //Reset button clicked
       cy.get('#reset-btn').should('be.enabled');
       cy.get('#reset-btn').trigger('click');
 
+      //Timer resets to 3000ms
       cy.get('#work-message').should('contain', 'Pomodoro - Start working!');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 3000);
@@ -98,20 +152,24 @@ describe('Timer Container Tests', () => {
 
       cy.tick(3100);
 
+      //Short Break after a 3100ms pause
       cy.get('#work-message').should('contain', 'Short Break - Good job!');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 3000);
       });
 
+      //Reset button disables during break
       cy.get('#reset-btn').should('be.disabled');
 
       cy.tick(3100);
 
+      //Work cycle after a 3100ms pause
       cy.get('#work-message').should('contain', 'Pomodoro - Start working!');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 3000);
       });
 
+      //Reset button re-enabled for work cycle
       cy.get('#reset-btn').should('be.enabled');
 
     });
@@ -119,30 +177,35 @@ describe('Timer Container Tests', () => {
 
   it('Test End Button functionality', () => {
 
+    //Task added so that session could be started for testing
+    cy.get('#task-btn').trigger('click');
     cy.get('task-list').within(() => {
       cy.get('#task-input').clear().type('First Test Task');
       cy.get('#add-btn').trigger('click');
+      cy.get('#close-task').trigger('click');
     });
 
     cy.get('timer-element').within(() => {
       cy.clock();
 
+      //Session started
       cy.get('#start-btn').trigger('click');
-
       cy.get('#work-message').should('contain', 'Pomodoro - Start working!');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 3000);
       });
 
+      //End button clicked during work cycle
       cy.get('#end-btn').trigger('click');
 
+      //Session ended
       cy.get('#work-message').should('contain', 'Ready to focus?');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', -1);
       });
 
+      //Session started
       cy.get('#start-btn').trigger('click');
-
       cy.get('#work-message').should('contain', 'Pomodoro - Start working!');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 3000);
@@ -150,50 +213,61 @@ describe('Timer Container Tests', () => {
 
       cy.tick(3100);
 
+      //Short Break after a 3100ms pause
       cy.get('#work-message').should('contain', 'Short Break - Good job!');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 3000);
       });
 
+      //End button clicked during break
       cy.get('#end-btn').trigger('click');
 
+      //Session Ended
       cy.get('#work-message').should('contain', 'Ready to focus?');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', -1);
       });
-
     });
   });
 
   it('Test Timer Countdown', () => {
 
+    //Task added so that session could be started for testing
+    cy.get('#task-btn').trigger('click');
     cy.get('task-list').within(() => {
       cy.get('#task-input').clear().type('First Test Task');
       cy.get('#add-btn').trigger('click');
+      cy.get('#close-task').trigger('click');
     });
 
     cy.get('timer-element').within(() => {
       cy.clock();
+
+      //Session started
       cy.get('#start-btn').trigger('click');
 
+      //Timer displays 3000ms (Work cycle duration)
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 3000);
       });
 
       cy.tick(1000);
 
+      //Timer runs down to 2000ms after a 1000ms pause
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 2000);
       });
 
       cy.tick(1000);
 
+      //Timer runs down to 1000ms after a 1000ms pause
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 1000);
       });
 
       cy.tick(1000);
 
+      //Timer runs down to 0ms after a 1000ms pause
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 0);
       });
@@ -202,25 +276,31 @@ describe('Timer Container Tests', () => {
 
   });
 
-  it('Test User Life Cycle', () => {
+  it('Test TimerContainer for the whole Pomodoro Life Cycle', () => {
 
+    //Task added so that session could be started for testing
+    cy.get('#task-btn').trigger('click');
     cy.get('task-list').within(() => {
       cy.get('#task-input').clear().type('First Test Task');
       cy.get('#add-btn').trigger('click');
+      cy.get('#close-task').trigger('click');
     });
 
     cy.get('timer-element').within(() => {
       
       cy.clock();
 
+      //Initially, Session not started
       cy.get('#work-message').should('contain', 'Ready to focus?');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', -1);
         expect($el).to.have.attr('pomos-comp', 0);
       });
 
+      //Start button clicked; Session started
       cy.get('#start-btn').trigger('click');
       
+      //Session enters Work cycle; Timer displays 3000ms (Work cycle duration); 0 pomos completed
       cy.get('#work-message').should('contain', 'Pomodoro - Start working!');
       cy.get('timer-display').then(function($el) {
         expect($el).to.have.attr('time', 3000);
@@ -228,7 +308,8 @@ describe('Timer Container Tests', () => {
       });
       
       cy.tick(3000);
-      
+
+      //Session still in Work Cycle after 3000ms; Timer displays 0ms
       cy.get('#work-message').should('contain', 'Pomodoro - Start working!');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 0);
@@ -237,6 +318,7 @@ describe('Timer Container Tests', () => {
 
       cy.tick(100);
 
+      //Session enters Short Break cycle; Timer displays 3000ms (Short break duration); 1 pomos completed
       cy.get('#work-message').should('contain', 'Short Break - Good job!');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 3000);
@@ -245,6 +327,7 @@ describe('Timer Container Tests', () => {
 
       cy.tick(3000);
 
+      //Session still in Short Break Cycle after 3000ms; Timer displays 0ms
       cy.get('#work-message').should('contain', 'Short Break - Good job!');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 0);
@@ -253,6 +336,7 @@ describe('Timer Container Tests', () => {
 
       cy.tick(100);
 
+      //Session enters Work cycle; Timer displays 3000ms (Work duration); 1 pomos completed
       cy.get('#work-message').should('contain', 'Pomodoro - Start working!');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 3000);
@@ -261,6 +345,7 @@ describe('Timer Container Tests', () => {
 
       cy.tick(3000);
 
+      //Session still in Work Cycle after 3000ms; Timer displays 0ms
       cy.get('#work-message').should('contain', 'Pomodoro - Start working!');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 0);
@@ -269,6 +354,7 @@ describe('Timer Container Tests', () => {
 
       cy.tick(100);
 
+      //Session enters Short Break cycle; Timer displays 3000ms (Short Break duration); 2 pomos completed
       cy.get('#work-message').should('contain', 'Short Break - Good job!');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 3000);
@@ -277,6 +363,7 @@ describe('Timer Container Tests', () => {
 
       cy.tick(3000);
 
+      //Session still in Short Break Cycle after 3000ms; Timer displays 0ms
       cy.get('#work-message').should('contain', 'Short Break - Good job!');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 0);
@@ -285,6 +372,7 @@ describe('Timer Container Tests', () => {
 
       cy.tick(100);
 
+      //Session enters Work cycle; Timer displays 3000ms (Work duration); 2 pomos completed
       cy.get('#work-message').should('contain', 'Pomodoro - Start working!');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 3000);
@@ -293,6 +381,7 @@ describe('Timer Container Tests', () => {
 
       cy.tick(3000);
 
+      //Session still in Work Cycle after 3000ms; Timer displays 0ms
       cy.get('#work-message').should('contain', 'Pomodoro - Start working!');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 0);
@@ -301,6 +390,7 @@ describe('Timer Container Tests', () => {
 
       cy.tick(100);
 
+      //Session enters Short Break cycle; Timer displays 3000ms (Short Break duration); 3 pomos completed
       cy.get('#work-message').should('contain', 'Short Break - Good job!');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 3000);
@@ -309,6 +399,7 @@ describe('Timer Container Tests', () => {
 
       cy.tick(3000);
 
+      //Session still in Short Break Cycle after 3000ms; Timer displays 0ms
       cy.get('#work-message').should('contain', 'Short Break - Good job!');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 0);
@@ -317,6 +408,7 @@ describe('Timer Container Tests', () => {
 
       cy.tick(100);
 
+      //Session enters Work cycle; Timer displays 3000ms (Work duration); 3 pomos completed
       cy.get('#work-message').should('contain', 'Pomodoro - Start working!');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 3000);
@@ -325,6 +417,7 @@ describe('Timer Container Tests', () => {
 
       cy.tick(3000);
 
+      //Session still in Work Cycle after 3000ms; Timer displays 0ms
       cy.get('#work-message').should('contain', 'Pomodoro - Start working!');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 0);
@@ -333,6 +426,7 @@ describe('Timer Container Tests', () => {
 
       cy.tick(100);
 
+      //Session enters Long Break cycle; Timer displays 3000ms (Long Break duration); 4 pomos completed
       cy.get('#work-message').should('contain', 'Long Break - Relax');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 3000);
@@ -341,6 +435,7 @@ describe('Timer Container Tests', () => {
 
       cy.tick(3000);
 
+      //Session still in Long Break Cycle after 3000ms; Timer displays 0ms
       cy.get('#work-message').should('contain', 'Long Break - Relax');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 0);
@@ -349,6 +444,7 @@ describe('Timer Container Tests', () => {
 
       cy.tick(100);
 
+      //Session enters Work cycle; Timer displays 3000ms (Work duration); pomos completed reset to 0 after the whole Pomodoro cycle ends
       cy.get('#work-message').should('contain', 'Pomodoro - Start working!');
       cy.get('timer-display').then(function ($el) {
         expect($el).to.have.attr('time', 3000);
@@ -357,7 +453,4 @@ describe('Timer Container Tests', () => {
     
     });
   });
-
-  
-
 });
