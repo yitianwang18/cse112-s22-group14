ce41ff16acb3fa0995f1b7f5a80d1066ccd99a1d
Xavier T``` <xavier-tan@hotmail.sg>
2021-03-15 15:16:44 -0700

    cleaned up style and comments

--- source/v1/jest_tests/taskDisplay.test.js
+++ source/v1/jest_tests/taskDisplay.test.js
@@ -1,15 +1,10 @@
 import { TaskDisplay } from "../js/taskDisplay.js";
 import { jest } from '@jest/globals';
 
-test('Testing attributeChangedCallback function to correctly alter task display', () => {
+test('Mock Testing attributeChangedCallback() to correctly alter taskdisplay', () => {
     let o_task_disp = new TaskDisplay();
-    // const mockCallBack = jest.fn(o_task_disp.attributeChangedCallback);
-    // o_task_disp.attributeChangedCallback = jest.fn();
-    // const mockCallback = jest.spyOn(o_task_disp, "attributeChangedCallback");
-    // expect(o_task_disp.attributeChangedCallback.mock.calls).toBe("numtasks");
-    // o_task_disp.attributeChangedCallback("numtasks", 0, 1);
 
-    const mockCallback = jest.spyOn(o_task_disp, "wrapperAttributeFunction");
+    const f_mockCallback = jest.spyOn(o_task_disp, "wrapperAttributeFunction");
 
     // task display should not show when no task
     o_task_disp.setAttribute("numtasks", 0);
@@ -17,73 +12,49 @@ test('Testing attributeChangedCallback function to correctly alter task display'
     expect(o_task_disp.querySelector("#next").style.display).toBe("none");
     expect(o_task_disp.getElementsByTagName("h3")[1].style.display).toBe("none");
     // The first arg of the first call to attributeChangedCallback to be "nexttask"
-    expect(mockCallback.mock.calls[0][0]).toBe("numtasks");
+    expect(f_mockCallback.mock.calls[0][0]).toBe("numtasks");
     // The second arg of the first call to attributeChangedCallback to be 0
-    expect(mockCallback.mock.calls[0][1]).toBe("0");
+    expect(f_mockCallback.mock.calls[0][1]).toBe("0");
     // We expect the function to be called once so far
-    expect(mockCallback).toHaveBeenCalledTimes(1);
+    expect(f_mockCallback).toHaveBeenCalledTimes(1);
 
     // task display should not show when only 1 task
     o_task_disp.setAttribute("numtasks", 1);
     expect(o_task_disp.querySelector("#next").style.display).toBe("none");
     expect(o_task_disp.getElementsByTagName("h3")[1].style.display).toBe("none");
     // The third arg of second call to attributeChangedCallback to be '1'
-    expect(mockCallback.mock.calls[1][2]).toBe("1");
+    expect(f_mockCallback.mock.calls[1][2]).toBe("1");
     // We expect the function to be called twice so far
-    expect(mockCallback).toHaveBeenCalledTimes(2);
+    expect(f_mockCallback).toHaveBeenCalledTimes(2);
 
     // task display should show when at least 2 tasks
     o_task_disp.setAttribute("numtasks", 2);
     expect(o_task_disp.querySelector("#next").style.display).toBe("");
     expect(o_task_disp.getElementsByTagName("h3")[1].style.display).toBe("");
     // The first arg of the third call to attributeChangedCallback was "nexttask"
-    expect(mockCallback.mock.calls[2][0]).toBe("numtasks");
+    expect(f_mockCallback.mock.calls[2][0]).toBe("numtasks");
     // The third arg of the third call to attributeChangedCallback to be 0
-    expect(mockCallback.mock.calls[2][2]).toBe("2");
-    expect(mockCallback).toHaveBeenCalledTimes(3);
+    expect(f_mockCallback.mock.calls[2][2]).toBe("2");
+    expect(f_mockCallback).toHaveBeenCalledTimes(3);
 
     // task display should reset current task to new task value
     o_task_disp.setAttribute("currtask", "Do Laundry");
     expect(o_task_disp.querySelector("#current").innerText).toBe("Do Laundry");
     // The first arg of the fourth call to attributeChangedCallback was "currtask"
-    expect(mockCallback.mock.calls[3][0]).toBe("currtask");
-    expect(mockCallback).toHaveBeenCalledTimes(4);
+    expect(f_mockCallback.mock.calls[3][0]).toBe("currtask");
+    expect(f_mockCallback).toHaveBeenCalledTimes(4);
 
     o_task_disp.setAttribute("nexttask", "Do Homework");
     expect(o_task_disp.querySelector("#next").innerText).toBe("Do Homework");
     // The first arg of the fifth call to attributeChangedCallback was "nexttask"
-    expect(mockCallback.mock.calls[4][0]).toBe("nexttask");
+    expect(f_mockCallback.mock.calls[4][0]).toBe("nexttask");
     // The third arg of the fifth call to attributeChangedCallback to be "Do Homework"
-    expect(mockCallback.mock.calls[4][2]).toBe("Do Homework");
+    expect(f_mockCallback.mock.calls[4][2]).toBe("Do Homework");
     // The function has been called 5 times so far
-    expect(mockCallback).toHaveBeenCalledTimes(5);
+    expect(f_mockCallback).toHaveBeenCalledTimes(5);
     // This function was instantiated five times so far
-    expect(mockCallback.mock.instances.length).toBe(5);
+    expect(f_mockCallback.mock.instances.length).toBe(5);
 
-    // let o_task_disp = new TaskDisplay();
-    //
-    // // task display should not show when no task
-    // o_task_disp.setAttribute("numtasks", 0);
-    // expect(o_task_disp.querySelector("#next").style.display).toBe("none");
-    //
-    // expect(o_task_disp.getElementsByTagName("h3")[1].style.display).toBe("none");
-    //
-    // // task display should not show when only 1 task
-    // o_task_disp.setAttribute("numtasks", 1);
-    // expect(o_task_disp.querySelector("#next").style.display).toBe("none");
-    // expect(o_task_disp.getElementsByTagName("h3")[1].style.display).toBe("none");
-    //
-    // // task display should show when at least 2 tasks
-    // o_task_disp.setAttribute("numtasks", 2);
-    // expect(o_task_disp.querySelector("#next").style.display).toBe("");
-    // expect(o_task_disp.getElementsByTagName("h3")[1].style.display).toBe("");
-    //
-    // // task display should reset current task to new task value
-    // o_task_disp.setAttribute("currtask", "Do Laundry");
-    // expect(o_task_disp.querySelector("#current").innerText).toBe("Do Laundry");
-    //
-    // o_task_disp.setAttribute("nexttask", "Do Homework");
-    // expect(o_task_disp.querySelector("#next").innerText).toBe("Do Homework");
 });
 
 test('Testing Task Display handleStartSession', () => {
