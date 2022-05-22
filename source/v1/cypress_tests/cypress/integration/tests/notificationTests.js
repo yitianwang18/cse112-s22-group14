2515dc1d1b081aa9ba038a38934591841d18f301
RishmalRohinkumar475 <57371405+RishmalRohinkumar@users.noreply.github.com>
2022-05-22 14:50:01 -0700

    Fixed Theme Keybind

--- source/v1/cypress_tests/cypress/integration/tests/notificationTests.js
+++ source/v1/cypress_tests/cypress/integration/tests/notificationTests.js
@@ -1,6 +1,6 @@
 describe("Notification Tests", () => {
     beforeEach(() => {
-        cy.visit("https://pomo-hero-dev.web.app/");
+        cy.visit("https://powelldoro.web.app/");
     });
 
     it("Tests alert when notifications are not supported", () => {
