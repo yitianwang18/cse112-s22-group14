cedef11dd28086026374c74a63438da3fe35f862
h4wang489 <h4wang@ucsd.edu>
2022-05-25 18:05:17 -0700

    finish task tests and local storage tests

--- source/v1/cypress_tests/cypress/integration/tests/taskTests.js
+++ source/v1/cypress_tests/cypress/integration/tests/taskTests.js
@@ -1,12 +1,13 @@
 describe("Test Tasks", () => {
     beforeEach(() => {
         cy.visit("https://powelldoro.web.app/");
+        cy.get('instructions-box > #instructions > .close2').click();
     });
 
-    it("Test add task + update task names", () => {
+    it("Test add task and update task", () => {
         // Testing add first task
         cy.get(".fa-tasks").trigger("click");
-        cy.get("input[name=task]").clear().type("Do Homework");
+        cy.get("input[placeholder]").clear().type("Do Homework");
         cy.get("input[type=text]").then(function ($o_el) {
             expect($o_el).to.have.value("Do Homework");
         });
@@ -14,24 +15,44 @@ describe("Test Tasks", () => {
 
         // add second task
         cy.get("input[placeholder]").type("Wash Dishes");
-        cy.get("input[type='text']").then(function ($o_el) {
+        cy.get("input[type=text]").then(function ($o_el) {
             expect($o_el).to.have.value("Wash Dishes");
         });
         cy.get("#add-btn").trigger("click");
 
         // Test that taskid is labelled correctly
-        cy.get("task-item").then(function ($o_el) {
-            expect($o_el).to.have.attr("taskid", 0);
+        cy.get("task-item[taskid='0']").then(function ($o_el) {
+            expect($o_el).to.have.attr("taskname", "Do Homework");
         });
-        cy.get("task-item").then(function ($o_el) {
-            expect($o_el).to.have.attr("taskid", 0);
+        cy.get("task-item[taskid='1']").then(function ($o_el) {
+            expect($o_el).to.have.attr("taskname", "Wash Dishes");
         });
 
+        // Test that local storage stores correctly
+        cy.reload();
+        cy.get(".fa-tasks").trigger("click");
+        cy.get("task-item[taskid='0']").then(function ($o_el) {
+            expect($o_el).to.have.attr("taskname", "Do Homework");
+        });
+        cy.get("task-item[taskid='1']").then(function ($o_el) {
+            expect($o_el).to.have.attr("taskname", "Wash Dishes");
+        });
+        
         // Testing edit/update task 1 attribute name
         cy.get("task-item[taskid='0'] input").clear().type("Do Laundry");
         cy.get("task-item[taskid='0'] input").blur();
         cy.get("task-item[taskid='0']").then(function ($o_el) {
             expect($o_el).to.have.attr("taskname", "Do Laundry");
+        });
+
+        // Test that local storage updates correctly
+        cy.reload();
+        cy.get(".fa-tasks").trigger("click");
+        cy.get("task-item[taskid='0']").then(function ($o_el) {
+            expect($o_el).to.have.attr("taskname", "Do Laundry");
+        });
+        cy.get("task-item[taskid='1']").then(function ($o_el) {
+            expect($o_el).to.have.attr("taskname", "Wash Dishes");
         });
 
         // Testing edit/update task 2 attribute name
@@ -41,14 +62,22 @@ describe("Test Tasks", () => {
             expect($o_el).to.have.attr("taskname", "Wash car");
         });
 
+        // Test that local storage updates correctly
+        cy.reload();
+        cy.get(".fa-tasks").trigger("click");
+        cy.get("task-item[taskid='0']").then(function ($o_el) {
+            expect($o_el).to.have.attr("taskname", "Do Laundry");
+        });
+        cy.get("task-item[taskid='1']").then(function ($o_el) {
+            expect($o_el).to.have.attr("taskname", "Wash car");
+        });
     });
 
-    it("Testin case: input task name is empty string or over character count", () => {
+    it("Test input task name is empty string or over character count", () => {
         // Testing add task
         cy.get(".fa-tasks").trigger("click");
         cy.get("input[type=text]").clear().type("Do Homework");
-        // cy.get("#add-task").get("input").clear().type("Do Homework");
-        cy.get("input[type='text']").then(function ($o_el) {
+        cy.get("input[type=text]").then(function ($o_el) {
             expect($o_el).to.have.value("Do Homework");
         });
         cy.get("#add-btn").trigger("click");
@@ -68,13 +97,25 @@ describe("Test Tasks", () => {
         cy.get("task-item[taskid='0'] input").then(function ($o_el) {
             expect($o_el).to.have.value("Do Homework");
         });
+        //Test local storage updates correctly
+        cy.reload();
+        cy.get(".fa-tasks").trigger("click");
+        cy.get("task-item[taskid='0'] input").then(function ($o_el) {
+            expect($o_el).to.have.value("Do Homework");
+        });
     });
 
-    it("test that the task name is trimmed when spaced input given", () => {
+    it("Test if the task name is trimmed when spaced input given", () => {
         // Testing add task
         cy.get(".fa-tasks").trigger("click");
         cy.get("input[type=text]").clear().type("      Do Homework       ");
         cy.get("#add-btn").trigger("click");
+        cy.get("task-item[taskid='0'] input").then(function ($o_el) {
+            expect($o_el).to.have.value("Do Homework");
+        });
+        //Test local storage updates correctly
+        cy.reload();
+        cy.get(".fa-tasks").trigger("click");
         cy.get("task-item[taskid='0'] input").then(function ($o_el) {
             expect($o_el).to.have.value("Do Homework");
         });
@@ -87,6 +128,57 @@ describe("Test Tasks", () => {
         cy.get("task-item[taskid='0'] input").then(function ($o_el) {
             expect($o_el).to.have.value("Do Laundry");
         });
+        //Test local storage updates correctly
+        cy.reload();
+        cy.get(".fa-tasks").trigger("click");
+        cy.get("task-item[taskid='0'] input").then(function ($o_el) {
+            expect($o_el).to.have.value("Do Laundry");
+        });
     });
+    it("Test add many tasks and edit part of them", () => {
+        // Testing add task
+        cy.get(".fa-tasks").trigger("click");
+        cy.get("input[placeholder]").clear().type("task0");
+        cy.get("#add-btn").trigger("click");
+        cy.get("input[placeholder]").clear().type("task1");
+        cy.get("#add-btn").trigger("click");
+        cy.get("input[placeholder]").clear().type("task2");
+        cy.get("#add-btn").trigger("click");
+        cy.get("input[placeholder]").clear().type("task3");
+        cy.get("#add-btn").trigger("click");
+        cy.get("input[placeholder]").clear().type("task4");
+        cy.get("#add-btn").trigger("click");
+        cy.get("input[placeholder]").clear().type("task5");
+        cy.get("#add-btn").trigger("click");
+        cy.get("input[placeholder]").clear().type("task6");
+        cy.get("#add-btn").trigger("click");
+        cy.get("task-item[taskid='3'] input").type("task333333");
+        cy.get("task-item[taskid='3'] input").blur();
+        cy.get("task-item[taskid='5'] input").type("task555555");
 
+        //Test local storage updates correctly
+        cy.reload();
+        cy.get(".fa-tasks").trigger("click");
+        cy.get("task-item[taskid='0'] input").then(function ($o_el) {
+            expect($o_el).to.have.value("task0");
+        });
+        cy.get("task-item[taskid='1'] input").then(function ($o_el) {
+            expect($o_el).to.have.value("task1");
+        });
+        cy.get("task-item[taskid='2'] input").then(function ($o_el) {
+            expect($o_el).to.have.value("task2");
+        });
+        cy.get("task-item[taskid='3'] input").then(function ($o_el) {
+            expect($o_el).to.have.value("task3task333333");
+        });
+        cy.get("task-item[taskid='4'] input").then(function ($o_el) {
+            expect($o_el).to.have.value("task4");
+        });
+        cy.get("task-item[taskid='5'] input").then(function ($o_el) {
+            expect($o_el).to.have.value("task5");
+        });
+        cy.get("task-item[taskid='6'] input").then(function ($o_el) {
+            expect($o_el).to.have.value("task6");
+        });
+    });
 });
