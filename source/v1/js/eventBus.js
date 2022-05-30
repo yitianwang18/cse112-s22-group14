b7a004438c964e03b11f1e56c5de292c5f7205a0
RishmalRohinkumar475 <57371405+RishmalRohinkumar@users.noreply.github.com>
2022-05-29 20:48:50 -0700

    Fixed issue from lint

--- source/v1/js/eventBus.js
+++ source/v1/js/eventBus.js
@@ -73,8 +73,7 @@ class EventBus {
     /**
      * Event handler function for the 'startSession' Event
      */
-    handleStartSession() {
-        let o_add_error = document.querySelector("#add-error");
+    handleStartSession() {let o_add_error = document.querySelector("#add-error");
         this.o_task_list.showTaskList();
         // check for valid application states
         if (this.o_task_list.getNumTasks() != 0 && 
@@ -88,17 +87,6 @@ class EventBus {
             this.o_task_list.closeTaskList();
             this.updateTaskDisplay();
             this.handleStartWork();
-            // Make sure the button can't be seen
-            document.querySelector("#close-task").style.visibility = "hidden";
-            
-            o_start_error.innerHTML = "";
-            o_start_error.classList.remove("color-error");
-            // Time must be the greater than the timeout for displaying the task list in taskList.js
-            setTimeout(() => {
-                document.querySelector("#close-task").style.display = "none";
-                document.querySelector("#task-title").style.display = "none";
-                document.querySelector("#all-tasks").style.display = "none";
-            }, 310);
         } else {
             // show start error message
             o_add_error.innerHTML = EventBus.S_START_ERROR;
